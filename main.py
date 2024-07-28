aa7d8987aa576c57d0fa737b759238d828b29691
shiamakd25 <shiamakdas@outlook.com>
2024-07-28 10:54:52 -0400

    Added additional information for the user

--- main.py
+++ main.py
@@ -11,3 +11,5 @@ with col2:
     st.title("Shiamak Das")
     description = """Hi, I am Shiamak. This is a project portfolio consisting of various Python projects spanning across many Python topics. These topics include PDF documents and operations, automating emails, managing and visualizing data, using APIs, machine learning, web scraping, OOP, SQL databases, web development with Django and Flask, data science, and Python packages."""
     st.info(description)
+
+st.write("Scroll down to view")
